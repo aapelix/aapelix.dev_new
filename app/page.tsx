1bcb3226a292df8238b2e0565cd43b90f3d9aeef
aapelix <mcaapelix@gmail.com>
2024-10-13 12:47:54 +0300

    major changes to ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,173 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 import Image from "next/image";
+import qr_image from "@/public/image.png"
+import img2 from "@/public/img2.png"
+import img3 from "@/public/img3.png"
+import img4 from "@/public/img4.png"
+import { Github, Twitter, Youtube } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [input, setInput] = useState("");
+  const [limit, setLimit] = useState(5);
+
+  const [repos, setRepos] = useState<any[]>([]);
+  const [fRepos, setFRepos] = useState<any[]>([]);
+
+  async function getRepos() {
+    const res = await fetch("https://api.github.com/users/aapelix/repos?per_page=60&sort=created");
+    const data = await res.json();
+
+    setRepos(data);
+  }
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
+  useEffect(() => {
+    const filtered = repos.filter((item) =>
+      item.name.toLowerCase().startsWith(input.toLowerCase()),
+    );
+
+    setFRepos(filtered);
+  }, [input, repos]);
+
+  return (
+    <div className="grid bg-black grid-rows-[20px_1fr_20px] items-center justify-items-center p-4 sm:p-8 lg:p-20 gap-8 sm:gap-16 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="mt-[40rem] lg:mt-16 md:mt-96 sm:mt-96">
+        <Carousel className="mt-[20rem] sm:mt-[40rem] lg:mt-[95rem] lg:block">
+          <CarouselContent>
+            <CarouselItem className="flex flex-col justify-center items-center">
+              <Image src={qr_image} alt="Showcase image of my QR code generator" className="rounded-3xl w-full sm:w-2/3 lg:w-1/2" />
+              <div className="mt-2 flex gap-2">
+                <p className="text-zinc-300">aapelix/qr - qr code generator</p>
+                <p className="text-zinc-300">|</p>
+                <a href="https://github.com/aapelix/qr" className="text-zinc-300 hover:underline">GitHub</a>
+                <a href="https://github.com/aapelix/qr/releases" className="text-zinc-300 hover:underline">Releases</a>
+              </div>
+            </CarouselItem>
+            <CarouselItem className="flex flex-col justify-center items-center">
+              <Image src={img4} alt="Showcase image of my QR code generator" className="rounded-3xl w-full sm:w-2/3 lg:w-1/2" />
+              <div className="mt-2 flex gap-2">
+                <p className="text-zinc-300">weather.aapelix.dev - weather app</p>
+                <p className="text-zinc-300">|</p>
+                <a href="https://github.com/aapelix/weather.aapelix.dev" className="text-zinc-300 hover:underline">GitHub</a>
+                <a href="https://weather.aapelix.dev" className="text-zinc-300 hover:underline">Site</a>
+              </div>
+            </CarouselItem>
+            
+            <CarouselItem className="flex flex-col justify-center items-center">
+              <Image src={img3} alt="Showcase image of my QR code generator" className="rounded-3xl w-full sm:w-2/3 lg:w-1/2" />
+              <div className="mt-2 flex gap-2">
+                <p className="text-zinc-300">blocksmined - minecraft mod to track total blocks mined</p>
+                <p className="text-zinc-300">|</p>
+                <a href="https://github.com/aapelix/blocksmined" className="text-zinc-300 hover:underline">GitHub</a>
+                <a href="https://modrinth.com/mod/blocksmined" className="text-zinc-300 hover:underline">Modrinth</a>
+              </div>
+            </CarouselItem>
+            <CarouselItem className="flex flex-col justify-center items-center">
+              <Image src={img2} alt="Showcase image of my QR code generator" className="rounded-3xl w-full sm:w-2/3 lg:w-1/2" />
+              <div className="mt-2 flex gap-2">
+                <p className="text-zinc-300">aocs - docs generator made with rust</p>
+                <p className="text-zinc-300">|</p>
+                <a href="https://github.com/aapelix/aocs" className="text-zinc-300 hover:underline">GitHub</a>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="translate-y-24 sm:translate-y-44 pb-16">
+          <h1 className="text-white font-extrabold text-4xl sm:text-6xl lg:text-9xl">It's me, aapelix</h1>
+          <h1 className="text-white font-extrabold text-4xl sm:text-6xl lg:text-9xl rotate-180">It's me, aapelix</h1>
+          
+          <div id="socials" className="w-full flex justify-center items-center mt-5 text-white gap-3 flex-wrap">
+            <a href="https://url.aapelix.dev/4rCO" className="bg-[#1a1a1a] rounded-3xl">
+              <button className="w-full sm:w-auto px-6 py-3 sm:py-6 flex gap-2 hover:px-16 sm:hover:px-28 duration-300">
+                <Github color="white" />
+                <p className="font-bold">GitHub</p>
+              </button>
+            </a>
+            <a href="https://url.aapelix.dev/eYMe" className="bg-[#1a1a1a] rounded-3xl">
+              <button className="w-full sm:w-auto px-6 py-3 sm:py-6 flex gap-2 hover:px-16 sm:hover:px-28 duration-300">
+                <Youtube color="white" />
+                <p className="font-bold">Youtube</p>
+              </button>
+            </a>
+            <a href="https://url.aapelix.dev/3rdE" className="bg-[#1a1a1a] rounded-3xl">
+              <button className="w-full sm:w-auto px-6 py-3 sm:py-6 flex gap-2 hover:px-16 sm:hover:px-28 duration-300">
+                <Twitter color="white" />
+                <p className="font-bold">Twitter</p>
+              </button>
+            </a>
+          </div>
+          
+          <div className="mt-10">
+            <input
+              type="search"
+              autoComplete="false"
+              className="focus:w-full w-56 duration-300 py-2 rounded-full px-5 bg-blue-500 placeholder:text-zinc-300"
+              placeholder="Search my projects"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <ul className="bg-white text-black rounded-3xl px-5 py-2 mt-2 max-h-[300px] overflow-y-auto">
+              {fRepos.slice(0, limit).map((project, index) => (
+                <li
+                  className="my-0.5 flex justify-between items-center"
+                  key={index}
+                >
+                  <div>
+                    <div className="flex flex-row gap-x-2">
+                      <h1 className="font-bold">{project.name}</h1>
+                      <p>{project.archived ? "(archived)" : ""}</p>
+                      <p>{project.fork ? "(fork)" : ""}</p>
+                    </div>
+                    {!project.description && (
+                      <p className="-translate-y-1">{project.full_name}</p>
+                    )}
+                    <p className="-translate-y-1">{project.description}</p>
+                  </div>
+                  <div className="flex gap-x-2">
+                    <p>{project.language ? project.language : "N/A"}</p>
+                    <a
+                      href={project.html_url}
+                      className="hover:scale-110 duration-300"
+                    >
+                      <Github />
+                    </a>
+                  </div>
+                </li>
+              ))}
+              <button
+                className="bg-blue-500 py-1 px-2 rounded-full text-white hover:scale-105 duration-300 disabled:cursor-not-allowed"
+                disabled={limit >= fRepos.length}
+                onClick={() => setLimit(limit + 3)}
+              >
+                Show more
+              </button>
+              <button
+                className="bg-blue-500 ml-2 py-1 px-2 rounded-full text-white hover:scale-105 duration-300 disabled:cursor-not-allowed"
+                disabled={ limit == 5}
+                onClick={() => setLimit(5)}
+              >
+                Show less
+              </button>
+            </ul>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
